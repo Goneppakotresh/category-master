854da8754ed552a087e97b762511ad810a8360d5
Goneppakotresh <90197239+Goneppakotresh@users.noreply.github.com>
2023-09-19 16:40:05 +0530

    Add files via upload

---
+++ Category.cs
@@ -0,0 +1,82 @@
+﻿using IIITS.JUNCTIONDB.DAL;
+using Oracle.ManagedDataAccess.Client;
+using System.Data;
+namespace CountryMasterForm.Models
+{
+    public class Category
+    {
+        #region Variables
+        public int categoryid { get; set; }
+        public string? categoryname { get; set; }
+        public string? description { get; set; }
+        public int responsecode { get; set; }
+        public string? responseMessage { get; set; }
+        #endregion
+        public int  isActive { get; set; }
+        #region Lists
+        public List<Category> lstdt = new List<Category>();
+        #endregion
+
+        /// <summary>
+        /// Save to Database/Update data to Database
+        /// </summary>
+        /// <param name="objDetails"></param>
+        /// <returns></returns>
+        public int SaveOrUpdateDetails(Category objDetails)
+        {
+            try
+            {
+                CustOledbConnection objcon = new CustOledbConnection("IIITS", "C:\\Users\\goneppa.gk\\Documents\\CountryMasterForm\\CountryMasterForm\\CountryMasterForm\\constring.json");
+                objcon.proc_name = "CATEGORY_MASTER_SAVE_UPDATE_DETAILS";
+                objcon.dtInputParameter.Rows.Add("C_CATEGORY_ID", Convert.ToInt32(objDetails.categoryid), "INPUT");
+                objcon.dtInputParameter.Rows.Add("C_CATEGORY_NAME", objDetails.categoryname, "INPUT");
+                objcon.dtInputParameter.Rows.Add("C_REMARK", objDetails.description, "INPUT");
+                objcon.dtInputParameter.Rows.Add("OUT_ID", responsecode, "OUTPUT");
+                objcon.ArrInputType[0] = OracleDbType.Int32;
+                objcon.ArrInputType[1] = OracleDbType.Varchar2;
+                objcon.ArrInputType[2] = OracleDbType.Varchar2;
+                objcon.ArrInputType[3] = OracleDbType.Int32;
+                string[] arr = objcon.Execute(objcon);
+                responsecode = Convert.ToInt32(arr[0]);
+                return responsecode;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Fetch DataTable
+        /// </summary>
+        /// <returns></returns>
+        public List<Category> FetchCategorydt()
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+                CustOledbConnection objcon = new CustOledbConnection("IIITS", "C:\\Users\\goneppa.gk\\Documents\\CountryMasterForm\\CountryMasterForm\\CountryMasterForm\\constring.json");
+                objcon.proc_name = "FETCH_CATEGORY_TABLE";
+                objcon.dtInputParameter.Rows.Add("PROC_OUTPUT", "", "OUTPUT");
+                objcon.ArrInputType[0] = OracleDbType.RefCursor;
+                dt = objcon.FetchDataTable(objcon);
+                if (dt.Rows.Count > 0)
+                {
+                    lstdt = (from dr in dt.AsEnumerable()
+                             select new Category()
+                             {
+                                 categoryid = Convert.ToInt32(dr["CATEGORY_ID"]),
+                                 categoryname = Convert.ToString(dr["CATEGORY_NAME"]),
+                                 description = Convert.ToString(dr["REMARK"]),
+                                 isActive = Convert.ToInt32(dr["IS_ACTIVE"]),
+                             }).ToList();
+                }
+                return lstdt;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ CategoryController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CountryMasterForm.Models;
+namespace CountryMasterForm.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        /// <summary>
+        /// F for Controller
+        /// </summary>
+        /// <returns></returns
+        public IActionResult Category()
+        {
+            Category objcagegory = new Category();
+            try
+            {
+                return View(objcagegory);
+            }
+            catch (Exception ex)
+            {
+                objcagegory.responsecode = -1;
+                objcagegory.responseMessage = "Something Went Wrong..!";
+                return Json(objcagegory);
+            }
+        }
+
+        /// <summary>
+        /// Accepting Details of obj and pass to Model/View
+        /// </summary>
+        /// <param name="objDetails"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Category(Category objDetails)
+        {
+            try
+            {
+                objDetails.responsecode = objDetails.SaveOrUpdateDetails(objDetails);
+                return View(objDetails);
+            }
+            catch (Exception ex)
+            {
+                objDetails.responsecode = -1;
+                objDetails.responseMessage = "Something Went Wrong..!";
+                return Json(objDetails);
+            }
+        }
+
+        /// <summary>
+        /// Binding for grid Datatable
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult GetCategoryDataTable()
+        {
+            Category objDt = new Category();
+            try
+            {
+                return Json(new { data = objDt.FetchCategorydt() });
+            }
+            catch (Exception ex)
+            {
+                objDt.responsecode = -1;
+                objDt.responseMessage = "Something Went Wrong..!";
+                return Json(objDt);
+            }
+        }
+    }
+}
